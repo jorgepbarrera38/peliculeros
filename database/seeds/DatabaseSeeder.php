2c69bfaec0cd61ce5b574a7830b83f068353f917
Jorge Alexis Paz Barrera <jorgepbarrera38@gmail.com>
2018-06-02 15:40:40 -0500

    Comentado seeds en database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(DirectorsTableSeeder::class);
+        /*$this->call(DirectorsTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
         $this->call(GendersTableSeeder::class);
-        $this->call(MoviesTableSeeder::class);
+        $this->call(MoviesTableSeeder::class);*/
         
     }
 }
